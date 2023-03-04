25ba878265e2a3be948e00dde64ef7b0ce53acfc
selva1406 <selva.nellai72@gmail.com>
2023-03-04 09:17:21 +0530

    Changes

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template, render_template_string, request, redirect, url_for, session
+from flask import Flask, render_template, request, redirect, url_for, session
 from flask_mysqldb import MySQL
 import hashlib, secrets
 import MySQLdb.cursors
