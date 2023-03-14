b3be2a6cc77bd39bbe43459931759ec3ddbc1c78
selva1406 <selva.nellai72@gmail.com>
2023-03-14 18:42:25 +0530

    New Changes

--- app.py
+++ app.py
@@ -196,7 +196,7 @@ def builderdash():
         image = acc[10]
         my_string = image.decode('utf-8')
         my_string_without_prefix = my_string.strip("'")
-        return render_template('builderdash.html',name = acc[1], comp=acc[5], image = my_string_without_prefix)
+        return render_template('builderprofile.html',name = acc[1], comp=acc[5],location = acc[7], image = my_string_without_prefix)
 
 
 @app.route('/userinfo', methods=['GET', 'POST'])
