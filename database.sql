a66718d5c33643a80f1c1b26fb9356d5e245d83b
selva1406 <selva.nellai72@gmail.com>
2023-03-19 11:41:54 +0530

    New

--- database.sql
+++ database.sql
@@ -1,9 +1,27 @@
 -- Create table for builder
-create table builder(id int primary key auto_increment, username varchar(50) not null, email varchar(100) not null, hashed_password varchar(100) not null, hashed_conpassword varchar(100) not null, companyname varchar(100) not null, industry varchar(100) not null, location varchar(100) not null, phone bigint(10) not null, reset_otp int, image blob not null);
+create table builder(id int primary key auto_increment, 
+                     username varchar(50) not null, 
+                     email varchar(100) not null, 
+                     hashed_password varchar(100) not null, 
+                     hashed_conpassword varchar(100) not null, 
+                     companyname varchar(100) not null, 
+                     industry varchar(100) not null, 
+                     location varchar(100) not null, 
+                     phone bigint(10) not null, 
+                     reset_otp int, 
+                     image blob not null);
 
 
 --Create table for users
-create table users(id int primary key auto_increment, username varchar(50) not null, email varchar(100) not null, hashed_password varchar(100) not null, hashed_conpassword varchar(100) not null, location varchar(50) not null, phone bigint(10) not null, reset_otp int, image blob not null);
+create table users(id int primary key auto_increment, 
+                   username varchar(50) not null, 
+                   email varchar(100) not null, 
+                   hashed_password varchar(100) not null, 
+                   hashed_conpassword varchar(100) not null, 
+                   location varchar(50) not null, 
+                   phone bigint(10) not null, 
+                   reset_otp int, 
+                   image blob not null);
 
 -- Bitting for user table
 create table bit(id int not null primary key auto_increment, 
